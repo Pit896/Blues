c6caf3857e32181e243200e4d1217ccae417c2e5
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-10-10 11:28:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { CanvasSenpai } = require('canvas-senpai');
 const canva = new CanvasSenpai();
 
 const db = require('quick.db');
-const { badwords } = require('./commands/json/badwords.json');
+//const { badwords } = require('./commands/json/badwords.json');
 
 const discord = require('discord.js');
 const client = new discord.Client({
