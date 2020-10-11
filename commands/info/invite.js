669ce86102946e4880b836202b6021b77a5d32bd
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-10-11 20:22:57 +0200

    Update invite.js

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -6,15 +6,14 @@ module.exports = {
     category: "info",
     description: "Invite link!",
     usage: "!invite",
-    run: (clint, message, args) => {
+    run: (client, message, args) => {
 
         let embed = new MessageEmbed()
         .setTitle(`INVITE ${clint.user.username} TO YOUR SERVER!`)
         .setFooter("Blues by Pit.js", `https://i.pinimg.com/originals/52/34/63/5234632e7e40c627035f42c3a064354f.jpg`)
         .setColor("#2518d9")
-        .setThumbnail(`https://www.publicdomainpictures.net/pictures/290000/velka/blue-swirl-background-1547523279to7.jpg`)
-        .setURL("https://discord.com/api/oauth2/authorize?client_id=755797708203229366&permissions=435547511&scope=bot")
-        .setDescription(`I'm a cool bot, pls invite me, most API, moderation, custom join and leave message!`);
+        .setThumbnail(client.user.displayAvatarURL())
+        .setDescription(`I'm a cool bot, pls invite me, most API, moderation, custom join and leave message (Press Me)[https://discord.com/api/oauth2/authorize?client_id=755797708203229366&permissions=435547511&scope=bot]`);
 
         message.channel.send(embed);
     }
