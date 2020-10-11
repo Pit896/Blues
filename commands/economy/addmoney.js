77ab5af60f36da28dc666d61e0a2c56a38bbfd39
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-10-11 21:25:59 +0200

    Update addmoney.js

--- commands/economy/addmoney.js
+++ commands/economy/addmoney.js
@@ -4,7 +4,7 @@ const db = require("quick.db");
 module.exports = {
   name: "add",
   description: "Add money from a member economy!",
-  usage: "!add <member> <amount>",
+  usage: "!add <member> <amount> <wallet/bank>",
   category: "economy",
   run: async (client, message, args) => {
 
@@ -19,14 +19,31 @@ module.exports = {
     if(!user) {
       return message.reply("you don't have mentioned a member to give money!");
   }
+
+    if(!args[1]) return message.reply("please specify a number!");
+
+    if(!args[2]) return message.reply("please specify bank/wallet!");   
   
-      if (isNaN(args[1])) return;
-      db.add(`money_${message.guild.id}_${user.id}`, args[1])
-  
-      let moneyEmbed = new Discord.MessageEmbed()
-      .setColor("#FFFFFF")
-      .setDescription(`✔Added ${args[1]} coins`);
-      message.channel.send(moneyEmbed)
+      if(args[2] == 'wallet') {
+        if (isNaN(args[1])) return message.reply("please specify a **valid** amount");
+        db.add(`money_${message.guild.id}_${user.id}`, args[1])
+    
+        let moneyEmbed = new Discord.MessageEmbed()
+        .setColor("#FFFFFF")
+        .setDescription(`✔Added ${args[1]} money in **${args[2]}**`);
+        message.channel.send(moneyEmbed)
+      }
+      
+      if(args[2] == 'bank') { 
+        if (isNaN(args[1])) return message.reply("please specify a **valid** amount");
+        db.add(`bank_${message.guild.id}_${user.id}`, args[1])
+    
+        let moneyEmbed = new Discord.MessageEmbed()
+        .setColor("#FFFFFF")
+        .setDescription(`✔Added ${args[1]} money in **${args[2]}**`);
+        message.channel.send(moneyEmbed)
+    }
   }
 }
+
 
