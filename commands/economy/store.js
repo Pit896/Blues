d574b1a8229f129cb38abd67c6f4d6f52b0653c0
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-10-12 15:20:05 +0200

    Update store.js

--- commands/economy/store.js
+++ commands/economy/store.js
@@ -10,7 +10,7 @@ module.exports = {
 
 
     let embed = new Discord.MessageEmbed()
-    .setDescription("**VIP Ranks**\n\n💎Bronze: 3500 Coins [!buy bronze]\n💎Silver: 4250 Coins [!buy silver]\n\n**Lifestyle Items**\n\n💸Fresh Nikes: 600 [!buy nikes]\n🏎Car: 800 [!buy car]\n🏢Mansion: 1200 [!buy mansion]")
+    .setDescription("**Lifestyle Items**\n\n💸Fresh Nikes: 600 [!buy nikes]\n🏎Car: 800 [!buy car]\n🏢Mansion: 1200 [!buy mansion]")
     .setColor("#FFFFFF")
     message.channel.send(embed)
 
