9893549d8fa3b9b12c39228712a316d5c8d82648
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-10-14 14:08:55 +0200

    Update invite.js

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -13,7 +13,7 @@ module.exports = {
         .setFooter("Blues by Pit.js", `https://i.pinimg.com/originals/52/34/63/5234632e7e40c627035f42c3a064354f.jpg`)
         .setColor("#2518d9")
         .setThumbnail(client.user.displayAvatarURL())
-        .setDescription(`I'm a cool bot, pls invite me, most API, moderation, custom join and leave message (Press Me)[https://discord.com/api/oauth2/authorize?client_id=755797708203229366&permissions=435547511&scope=bot]`);
+        .setDescription(`I'm a cool bot, pls invite me, most API, moderation, custom join and leave message [Press Me](https://discord.com/api/oauth2/authorize?client_id=755797708203229366&permissions=435547511&scope=bot)`);
 
         message.channel.send(embed);
     }
